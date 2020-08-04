e1006da702de001c95069f1613c56918be2769fa
druzb <druzb@DESKTOP-PT0FQJD>
2020-08-04 17:43:32 -0400

    added files

---
+++ Android/src/main/java/IfElse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author druzb
+ */
+public class IfElse {
+    public static void main(String[] args) {
+    int myNum = -10;
+    if (myNum > 0) {
+        System.out.println("The number is positive");
+    } else { 
+        System.out.println("The number is negative");
+    }
+    }
+    
+}

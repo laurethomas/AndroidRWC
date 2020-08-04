a7333bda6ddf63d190bc7326c19244710bbb2e4f
druzb <druzb@DESKTOP-PT0FQJD>
2020-08-04 18:03:25 -0400

    added files

---
+++ Android/src/main/java/Ladder.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author druzb
+ */
+public class Ladder {
+    public static void main(String[] args) {
+    int myNum = 0;
+    if (myNum > 0) {
+      System.out.println("The number is positive.");
+    } else if (myNum < 0) {
+      System.out.println("The number is negative.");
+    } else { 
+      System.out.println("The number is zero.");
+            }
+    
+    
+    }
+    }
+   

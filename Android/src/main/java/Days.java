0589e414ef824768afc876ab71ef04b2ee9d355a
druzb <druzb@DESKTOP-PT0FQJD>
2020-08-04 18:22:35 -0400

    added files

---
+++ Android/src/main/java/Days.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author druzb
+ */
+public class Days {
+    public static void main(String[] args) {
+    int myNum = 4;
+   
+    
+    switch (myNum) {
+        case 1:
+          System.out.println("The day is Monday");
+          break;
+          
+        case 2:
+          System.out.println("The day is Tuesday");
+          break;
+          
+        case 3: 
+          System.out.println("The day is Wenesday");
+          break;
+          
+        case 4:
+          System.out.println("The day is Thursday");
+          break;
+          
+        case 5:
+          System.out.println("The day is Friday");
+          break;
+          
+        case 6:
+          System.out.println("The day is Saturday");
+          break;
+          
+        case 7: 
+          System.out.println("The day is Sunday");
+          break; 
+    }
+}
+}

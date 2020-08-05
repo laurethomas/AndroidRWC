45eded5492dfb3824af54b2de08bfd0a51882511
druzb <druzb@DESKTOP-PT0FQJD>
2020-08-05 12:03:19 -0400

    added more files

---
+++ Android/src/main/java/Repeat.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author druzb
+ */
+public class Repeat {
+    public static void main(String[] args) {
+    
+        for (int i = 1; i <= 100; ++i) {
+            System.out.println("Yes " + i);
+        }
+    }
+    
+}

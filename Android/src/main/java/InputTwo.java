5e02ef1380e3d294fc4c51cf0816fd262092a9e7
druzb <druzb@DESKTOP-PT0FQJD>
2020-08-05 10:43:20 -0400

    changed files

--- Android/src/main/java/InputTwo.java
+++ Android/src/main/java/InputTwo.java
@@ -14,16 +14,16 @@ public class InputTwo {
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     
-    float question1 = input.nextFloat();
     System.out.print("How many liters of water do you drink a day?");
+    float question1 = input.nextFloat();
     System.out.print("You wrote " + question1);
     
-    double question2 = input.nextDouble();
     System.out.print("How old are you?");
+    double question2 = input.nextDouble();
     System.out.print("You wrote " + question2);
      
-    String question3 = input.next();
     System.out.print("Are you having a good day?");
+    String question3 = input.next();
     System.out.print("You wrote " + question3);
     
     input.close();

a706390cc7709b5119bd3fd38a74c64a626e07ca
druzb <druzb@DESKTOP-PT0FQJD>
2020-08-05 14:15:14 -0400

    added more files

---
+++ Android/src/main/java/MultiArray.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author druzb
+ */
+public class MultiArray {
+    public static void main(String[] args) {
+        
+        int[][] myNumbers = {
+            {2,4,6,8},
+            {1,3,5,7,9},
+            {10}
+           
+        };
+            
+        System.out.println("Length of row 1:" + myNumbers[0].length);
+        System.out.println("Length of row 2:" + myNumbers[1].length);
+        System.out.println("Length of row 3:" + myNumbers[2].length);
+    }
+    
+}
